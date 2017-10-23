b744338c0b4d834e81f96f4506156cc09b84e8b4
Aspen Humphrey <aspenjadedimmick@gmail.com>
2017-10-23 12:28:12 -0500

    task 38- get a single products details is now finished.

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -1,15 +1,26 @@
 'use strict';
 
 module.exports.getProduct = (req, res, next) => {
-  console.log("product");
   const { Products } = req.app.get('models');
   Products.findAll({ order: [['date_created', 'DESC']], limit: 20 })
   .then( ( product ) => {
     res.send(JSON.stringify( product ));
     // res.render('product', { product });
   })
-  .catch( (err) => {
-    next(err);
+  .catch( ( err ) => {
+    next( err );
+  });
+};
+
+module.exports.getProductDetail = (req, res, next) => {
+  const { Products } = req.app.get('models');
+  Products.findOne({ raw: true, where: {id: req.params.id} })
+  .then( ( product ) => {
+    res.send(JSON.stringify( product ));
+    // res.render('product', { product });
+  })
+  .catch( ( err ) => {
+    next( err );
   });
 };
 
